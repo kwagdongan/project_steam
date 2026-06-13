47eb1ba9c919a7a089a5485042c8e0c5d5b64dac
kwagdongan <142889476+kwagdongan@users.noreply.github.com>
2026-06-13 17:20:22 +0900

    Update ‎streamlit_app.py

--- ‎streamlit_app.py
+++ ‎streamlit_app.py
@@ -3,8 +3,8 @@ import pandas as pd
 
 st.set_page_config(page_title="Steam 분석", layout="wide")
 
-st.title("Steam 게임 데이터 분석")
-st.write("사이드바에서 분석 페이지를 선택하세요.")
+st.title("제목 테스트 중")
+st.write("사이드바")
 
 @st.cache_data
 def load_data():
