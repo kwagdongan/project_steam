e4cf75b373672e7ad07e8569c0925774342e06e0
kwagdongan <142889476+kwagdongan@users.noreply.github.com>
2026-06-13 19:32:15 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,12 +6,3 @@ st.set_page_config(page_title="Steam 분석", layout="wide")
 st.title("제목 테스트 중")
 st.write("사이드바")
 
-try:
-    @st.cache_data
-    def load_data():
-        return pd.read_csv("datas.csv", encoding='utf-8')
-    
-    df = load_data()
-    st.metric("전체 데이터 개수", f"{df.shape[0]:,} 개")
-except Exception as e:
-    st.error(f"데이터 로드 실패: {e}")
